669346ff3c13564c8e40754406f1eda2fc44985a
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-19 12:34:58 +0100

    feat: added NativeStackNavigationProp

--- src/ui/screens/home/home.screen.tsx
+++ src/ui/screens/home/home.screen.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {NavigatorStackParamList, Screen} from "../../navigation/types";
+
+interface Props {
+  navigation: NativeStackNavigationProp<NavigatorStackParamList, Screen.Home>
+}
 
 const HomeScreen = () => {
   return (
