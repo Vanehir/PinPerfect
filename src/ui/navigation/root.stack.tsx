63caa6c2a86ec1d4bf1bf99bfe106e61823a2b3b
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-22 15:21:00 +0100

    fix: changed names stack parameters

--- src/ui/navigation/root.stack.tsx
+++ src/ui/navigation/root.stack.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { DetailParamList } from './types';
+import { NavigatorStackParamList } from './types';
 import { Screen } from './types';
 import TabNavigator from './tab/tab.navigator';
 
-const Stack = createNativeStackNavigator<DetailParamList>();
+const Stack = createNativeStackNavigator<NavigatorStackParamList>();
 const RootStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
