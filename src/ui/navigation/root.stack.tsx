6e7b886fa4a877aa1584300936bb6eac24df6b9a
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-22 12:43:39 +0100

    fix: minor naming fixes

--- src/ui/navigation/root.stack.tsx
+++ src/ui/navigation/root.stack.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { DetailParamList } from '../types';
-import { Screen } from '../types';
-import TabNavigator from './tab.navigator';
+import { NavigatorStackParamList, Screen } from './types';
+import TabNavigator from './tab/tab.navigator';
 
-const Stack = createNativeStackNavigator<DetailParamList>();
+const Stack = createNativeStackNavigator<NavigatorStackParamList>();
 const RootStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
