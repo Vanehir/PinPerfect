2e1b8a8defdc65cdabdb9997c3768db896207bc8
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-23 17:55:13 +0100

    feat: added screen detail to root stack navigation

--- src/ui/navigation/root.stack.tsx
+++ src/ui/navigation/root.stack.tsx
@@ -4,12 +4,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigatorStackParamList } from './types';
 import { Screen } from './types';
 import TabNavigator from './tab/tab.navigator';
+import DetailScreen from '../screens/detail/detail.screen';
 
 const Stack = createNativeStackNavigator<NavigatorStackParamList>();
 const RootStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name={Screen.TabNavigator} component={TabNavigator} />
+      <Stack.Screen name={Screen.Detail} component={DetailScreen} />
     </Stack.Navigator>
   );
 };
