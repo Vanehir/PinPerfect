9e44f6888ddd5890541b974a398a4bd5b7c48c42
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-23 19:45:03 +0100

    feat: moved renderItem useCallbacks to useProducts

--- src/ui/screens/home/home.screen.tsx
+++ src/ui/screens/home/home.screen.tsx
@@ -1,85 +1,79 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-import { FlatList, ListRenderItem, View } from 'react-native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { NavigatorStackParamList, Screen } from '../../navigation/types';
-import { Product, useProducts } from '../hook/useProducts.facade';
-import ProductCard from '../../atoms/product/product.atom';
+import { FlatList, View } from 'react-native';
+import { useProducts } from '../hook/useProducts.facade';
 import API_URL from '../../../constants/api_urls';
-import Chip from '../../atoms/chip/chip.atom';
+import { HomeScreenProps } from '../../navigation/navigationProps';
 
-interface Props {
-  navigation: NativeStackNavigationProp<NavigatorStackParamList, Screen.Home>;
-}
-
-const HomeScreen = ({ navigation }: Props) => {
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
   const {
     products,
-    favoriteIds,
     getProducts,
     fetchCategories,
     categories,
-    selectedCategories,
-    setSelectedCategories,
-    filterProducts,
+    renderItemProduct,
+    renderItemCategory,
+    navigationProp,
+    setNavigationProp,
     loadFavorites,
-    addFavorite,
   } = useProducts();
 
   // ** USE CALLBACK ** //
-  const renderItemProduct = useCallback<ListRenderItem<Product>>(
-    ({ item }) => (
-      <ProductCard
-        product={item}
-        selected={favoriteIds.includes(item.id)}
-        onAddFavorite={() => addFavorite(item)}
-        onPress={() => {
-          if (!item.id) {
-            return;
-          }
-          navigation.navigate(Screen.Detail, {
-            id: item.id,
-            idsArray: products.map((el) => el.id),
-          });
-        }}
-      />
-    ),
-    [addFavorite, products, favoriteIds, navigation]
-  );
 
-  const renderItemCategory = useCallback<ListRenderItem<string>>(
-    ({ item }) => (
-      <Chip
-        title={item}
-        selected={selectedCategories.includes(item)}
-        onPress={() => {
-          if (selectedCategories.includes(item)) {
-            setSelectedCategories((currentSelectedCategories: string[]) =>
-              currentSelectedCategories.filter((category) => category !== item)
-            );
-          } else {
-            setSelectedCategories((currentSelectedCategories: string[]) => [
-              ...currentSelectedCategories,
-              item,
-            ]);
-            filterProducts;
-          }
-        }}
-      />
-    ),
-    [filterProducts, selectedCategories, setSelectedCategories]
-  );
+  // const renderItemProduct = useCallback<ListRenderItem<Product>>(
+  //   ({ item }) => (
+  //     <ProductCard
+  //       product={item}
+  //       selected={favoriteIds.includes(item.id)}
+  //       onAddFavorite={() => addFavorite(item)}
+  //       onPress={() => {
+  //         if (!item.id) {
+  //           return;
+  //         }
+  //         navigation.navigate(Screen.Detail, {
+  //           id: item.id,
+  //           idsArray: products.map((el) => el.id),
+  //         });
+  //       }}
+  //     />
+  //   ),
+  //   [addFavorite, products, favoriteIds]
+  // );
+
+  // const renderItemCategory = useCallback<ListRenderItem<string>>(
+  //   ({ item }) => (
+  //     <Chip
+  //       title={item}
+  //       selected={selectedCategories.includes(item)}
+  //       onPress={() => {
+  //         if (selectedCategories.includes(item)) {
+  //           setSelectedCategories((currentSelectedCategories: string[]) =>
+  //             currentSelectedCategories.filter((category) => category !== item)
+  //           );
+  //         } else {
+  //           setSelectedCategories((currentSelectedCategories: string[]) => [
+  //             ...currentSelectedCategories,
+  //             item,
+  //           ]);
+  //           filterProducts;
+  //         }
+  //       }}
+  //     />
+  //   ),
+  //   [filterProducts, selectedCategories, setSelectedCategories]
+  // );
 
   // ** USE EFFECT ** //
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      console.log('Favorites screen focused');
+      console.log('Home screen focused');
       getProducts(API_URL.BASE_URL); // TODO find out why this give an error even if category is optional
       fetchCategories(API_URL.CATEGORY_URL);
       loadFavorites();
+      setNavigationProp(navigation);
     });
     return unsubscribe;
-  }, []);
+  }, [navigation]);
 
   return (
     <>
