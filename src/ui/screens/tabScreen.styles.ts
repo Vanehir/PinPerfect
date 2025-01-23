e71d8f87b477580dd16372c4e75d602022168de8
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-23 23:14:21 +0100

    feat: created styles for home and favorite screen

--- src/ui/screens/tabScreen.styles.ts
+++ src/ui/screens/tabScreen.styles.ts
@@ -4,15 +4,22 @@ const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
     marginHorizontal: 10,
-    marginBottom: 60,
   },
   categoryContainer: {
     padding: 10,
+  },
+  productContainer: {
+    flex: 1,
   },
   noFavoritesContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  sortButtonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    zIndex: 1,
   },
 });
 
