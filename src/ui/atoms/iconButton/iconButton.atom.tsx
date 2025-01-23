970daf4f3b979d6036ca2160ad0b0710b3f73059
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-23 22:49:49 +0100

    feat: changed props iconButton

--- src/ui/atoms/iconButton/iconButton.atom.tsx
+++ src/ui/atoms/iconButton/iconButton.atom.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import styles from './iconButton.styles';
+import COLORS from '../../../constants/colors';
 
 const IconButton = ({
   icon,
   onPress,
-  color = 'white',
+  colorIcon = COLORS.WHITE,
+  colorButton = COLORS.TURQUOISE_SURF,
   disabled = false,
 }: {
   icon: keyof typeof Ionicons.glyphMap;
   onPress: () => void;
-  color?: string;
+  colorIcon?: string;
+  colorButton?: string;
   disabled?: boolean;
 }) => {
   return (
-    <TouchableOpacity style={styles.iconButton} onPress={onPress} disabled={disabled}>
-      <Ionicons name={icon} size={24} color={color} />
+    <TouchableOpacity
+      style={[styles.iconButton, { backgroundColor: colorButton }]}
+      onPress={onPress}
+      disabled={disabled}>
+      <Ionicons name={icon} size={24} color={colorIcon} />
     </TouchableOpacity>
   );
 };
