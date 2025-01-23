ef8a5aeb2b2c48110e7aa6bcccefc910b7777d9a
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-23 19:46:00 +0100

    feat: added useMemo filter for products

--- src/ui/screens/favorites/favorites.screen.tsx
+++ src/ui/screens/favorites/favorites.screen.tsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { NavigatorStackParamList, Screen } from '../../navigation/types';
+import React, { useEffect, useMemo } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import { useProducts } from '../hook/useProducts.facade';
 import API_URL from '../../../constants/api_urls';
+import { FavoritesScreenProps } from '../../navigation/navigationProps';
 
-interface Props {
-  navigation: NativeStackNavigationProp<NavigatorStackParamList, Screen.Favorites>;
-}
+const FavoritesScreen = ({ navigation }: FavoritesScreenProps) => {
+  const {
+    products,
+    favoriteIds,
+    getProducts,
+    fetchCategories,
+    categories,
+    renderItemProduct,
+    renderItemCategory,
+    navigationProp,
+    setNavigationProp,
+    loadFavorites,
+  } = useProducts();
 
-const FavoritesScreen = ({ navigation }: Props) => {
-  const { getProducts, fetchCategories, loadFavorites } = useProducts();
+  // ** USE MEMO ** //
+  const favorites = useMemo(
+    () => products.filter((product) => favoriteIds.includes(product.id)),
+    [products, favoriteIds]
+  );
 
   // ** USE EFFECT ** //
   useEffect(() => {
@@ -19,14 +31,29 @@ const FavoritesScreen = ({ navigation }: Props) => {
       getProducts(API_URL.BASE_URL); // TODO find out why this give an error even if category is optional
       fetchCategories(API_URL.CATEGORY_URL);
       loadFavorites();
+      setNavigationProp(navigation);
     });
+    console.log(navigationProp);
     return unsubscribe;
-  }, []);
+  }, [navigation]);
 
   return (
-    <View>
-      <Text>Favorites Screen placeholder</Text>
-    </View>
+    <>
+      <View>
+        <FlatList
+          data={categories}
+          renderItem={renderItemCategory}
+          keyExtractor={(item) => item}
+          horizontal
+        />
+
+        <FlatList
+          data={favorites}
+          renderItem={renderItemProduct}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+    </>
   );
 };
 
