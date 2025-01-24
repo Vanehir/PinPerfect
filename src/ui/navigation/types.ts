8d8a61ad976f114e4b2a76e5a86606fd5603afd7
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2025-01-24 14:26:12 +0100

    feat: added TabParams

--- src/ui/navigation/types.ts
+++ src/ui/navigation/types.ts
@@ -1,9 +1,20 @@
+import { SortType } from '../screens/hook/useProducts.facade';
+
 export enum Screen {
   TabNavigator = 'TabNavigator',
   Home = 'Home',
   Detail = 'Detail',
   Favorites = 'Favorites',
 }
+
+export type TabParams = {
+  [Screen.Home]: {
+    hasFavoritesUpdated: boolean;
+  };
+  [Screen.Favorites]: {
+    hasFavoritesUpdated: boolean;
+  };
+};
 
 export type NavigatorStackParamList = {
   TabNavigator: undefined;
